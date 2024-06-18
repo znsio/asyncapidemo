b6cc0858a12ef8b4b7df582ba934db223193778e
Hari Krishnan <harikrishnan83@gmail.com>
2024-06-18 10:10:26 +0530

    Adding ReceiveReplyAction

--- src/main/java/com/tmforum/asyncapidemo/receiver/ReceiveReplyAction.java
+++ src/main/java/com/tmforum/asyncapidemo/receiver/ReceiveReplyAction.java
@@ -1,4 +1,21 @@
 package com.tmforum.asyncapidemo.receiver;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
 public class ReceiveReplyAction {
+    @Autowired
+    KafkaTemplate<Integer, Object> kafkaTemplate;
+
+    @KafkaListener(topics = "troubleTicket.v5.createTroubleTicket.commandReply", groupId = "troubleTicketDemo")
+    public void listenCreateTroubleTicketRequest(@Payload String message, @Header("X-Correlation-Id") String correlationId) {
+        System.out.println("Received Create Trouble Ticket Reply with correlationId: " + correlationId + " and payload " + message);
+    }
 }
